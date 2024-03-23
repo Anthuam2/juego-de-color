936afa9766cd812d151159cbbe7b67ba689ad4d0
Anthuam <156540281+Anthuam2@users.noreply.github.com>
2024-03-23 18:38:55 -0400

    Add files via upload

--- juegoDeColores.js
+++ juegoDeColores.js
@@ -1,13 +1,14 @@
-const div1 = document.getElementById("div1");
-const div2 = document.getElementById("div2");
-const div3 = document.getElementById("div3");
+const div1 = document.querySelector("#div1");
+const div2 = document.querySelector("#div2");
+const div3 = document.querySelector("#div3");
 const colorFinal = document.querySelector(".color--final");
 let combinacion = [];
 
 const eliminarElemento = elemento => {
     const index = combinacion.indexOf(elemento);
     combinacion.splice(index, 1);
-    colorFinal.style.background = "#ffffff";
+    
+    colorFinal.style.background = "#FFFFFF";
 };
 
 const combinarColor = () => {
@@ -15,57 +16,44 @@ const combinarColor = () => {
         colorFinal.style.background = "rgb(129, 99, 15)";
     } else if (combinacion.includes("A") && combinacion.includes("B")) {
         colorFinal.style.background = "rgb(0, 176, 59)";
-    } else if (combinacion.includes("B") && combinacion.includes("C")) {
-        colorFinal.style.background = "rgb(97, 0, 145)";
     } else if (combinacion.includes("A") && combinacion.includes("C")) {
+        colorFinal.style.background = "rgb(97, 0, 145)";
+    } else if (combinacion.includes("B") && combinacion.includes("C")) {
         colorFinal.style.background = "rgb(255, 145, 1)";
     }
 };
 
-div1.addEventListener("click", e => {
+div1.addEventListener("click", () => {
     div1.classList.toggle("div--active");
-
     if (combinacion.includes("A")) {
         eliminarElemento("A");
-
         combinarColor();
         return;
     }
-    if (div1.classList.contains("div--active")) {
-        combinacion.push("A");
-    }
+    combinacion.push("A");
     combinacion.sort();
     combinarColor();
 });
-
-div2.addEventListener("click", e => {
+div2.addEventListener("click", () => {
     div2.classList.toggle("div--active");
-
     if (combinacion.includes("B")) {
         eliminarElemento("B");
-
         combinarColor();
         return;
     }
-    if (div2.classList.contains("div--active")) {
-        combinacion.push("B");
-    }
+
+    combinacion.push("B");
     combinacion.sort();
     combinarColor();
 });
-
-div3.addEventListener("click", e => {
+div3.addEventListener("click", () => {
     div3.classList.toggle("div--active");
-
     if (combinacion.includes("C")) {
         eliminarElemento("C");
-
         combinarColor();
         return;
     }
-    if (div3.classList.contains("div--active")) {
-        combinacion.push("C");
-    }
+    combinacion.push("C");
     combinacion.sort();
     combinarColor();
 });
